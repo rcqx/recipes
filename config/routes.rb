d670837257260e2e0fd87b90cd675be0cf465fb4
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-06 22:45:49 +0500

    Add an action that creates Food object

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'foods/new'
+  get 'foods/create'
+  get 'foods/destroy'
+  get 'foods/index'
+  get 'foods/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
