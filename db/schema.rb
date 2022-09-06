c803791c5160faea75b6d1df8af9860d0fa713b5
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-06 10:33:18 +0500

    setup database schema for food and recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_052430) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_053101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.integer "price"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
+  create_table "foods_recipes", id: false, force: :cascade do |t|
+    t.bigint "food_id", null: false
+    t.bigint "recipe_id", null: false
+    t.integer "quantity"
+    t.index ["food_id", "recipe_id"], name: "index_foods_recipes_on_food_id_and_recipe_id"
+    t.index ["recipe_id", "food_id"], name: "index_foods_recipes_on_recipe_id_and_food_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -22,6 +41,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_052430) do
     t.boolean "is_public"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_052430) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end
