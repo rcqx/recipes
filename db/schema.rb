e6dbf3742de51741fd89bd42e98ae8165717d651
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-07 01:09:09 +0500

    comment-out db password

--- db/schema.rb
+++ db/schema.rb
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_172147) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
