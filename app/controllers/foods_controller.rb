4da35771bc3bd525a23d6a9e4e5b6ca70fd4ab88
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-06 22:54:07 +0500

    Add navigation link for foods and recipe

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,14 +1,16 @@
 class FoodsController < ApplicationController
-  def index
-    @foods = current_user.foods
-  end
+    # load_and_authorize_resource
 
-  def new
-    render :new
-  end
-
-  def create
-    new_food = current_user.foods.new(food_params)
+    def index
+      @foods = current_user.foods
+    end
+  
+    def new
+      render :new
+    end
+  
+    def create
+      new_food = current_user.foods.new(food_params)
       if new_food.save
         flash[:success] = 'You successfully added a food item'
         redirect_to foods_url
@@ -16,13 +18,13 @@ class FoodsController < ApplicationController
         flash.now[:error] = 'Error: You could not add a food item'
         render :new
       end
-  end
-
-  def destroy; end
-
-  private
-
-  def food_params
-    params.require(:add_food).permit(:name, :measurement_unit, :price)
-  end
+    end
+  
+    def destroy; end
+  
+    private
+  
+    def food_params
+      params.require(:add_food).permit(:name, :measurement_unit, :price)
+    end
 end
