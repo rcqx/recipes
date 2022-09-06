9bf16caa8fe2f35791bc0ff7226ab1631ccaa8c9
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-06 22:56:14 +0500

    Fix linter errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,30 +1,30 @@
 class FoodsController < ApplicationController
-    # load_and_authorize_resource
+  # load_and_authorize_resource
 
-    def index
-      @foods = current_user.foods
-    end
-  
-    def new
+  def index
+    @foods = current_user.foods
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    new_food = current_user.foods.new(food_params)
+    if new_food.save
+      flash[:success] = 'You successfully added a food item'
+      redirect_to foods_url
+    else
+      flash.now[:error] = 'Error: You could not add a food item'
       render :new
     end
-  
-    def create
-      new_food = current_user.foods.new(food_params)
-      if new_food.save
-        flash[:success] = 'You successfully added a food item'
-        redirect_to foods_url
-      else
-        flash.now[:error] = 'Error: You could not add a food item'
-        render :new
-      end
-    end
-  
-    def destroy; end
-  
-    private
-  
-    def food_params
-      params.require(:add_food).permit(:name, :measurement_unit, :price)
-    end
+  end
+
+  def destroy; end
+
+  private
+
+  def food_params
+    params.require(:add_food).permit(:name, :measurement_unit, :price)
+  end
 end
