db041804ae0994881853abf997af3c0b37de1d93
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-06 10:35:49 +0500

    create many to many relationship between food and recipe

---
+++ db/migrate/20220906053457_create_recipe_foods.rb
@@ -0,0 +1,9 @@
+class CreateRecipeFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipe_foods do |t|
+      t.integer :quantity
+      t.belongs_to :recipe, foreign_key: true
+      t.belongs_to :food, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_053101) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_053457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_053101) do
     t.integer "quantity"
     t.index ["food_id", "recipe_id"], name: "index_foods_recipes_on_food_id_and_recipe_id"
     t.index ["recipe_id", "food_id"], name: "index_foods_recipes_on_recipe_id_and_food_id"
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "recipe_id"
+    t.bigint "food_id"
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
+    t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -54,5 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_053101) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "recipe_foods", "foods"
+  add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
 end
