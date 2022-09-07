735fb2c553d11dd15b7e00eae7b1bab3e1800504
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-07 14:08:57 +0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,19 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   get 'home/read'
   get 'home/shoppin_list'
-  get 'test/index'
-  get 'test/read'
-  get 'test/shoppingList'
-  get 'home/new'
-  get 'home/create'
-  get 'home/destroy'
-  get 'home/index'
-  get 'home/show'
-  get 'test/new'
-  get 'test/create'
-  get 'test/destroy'
-  get 'test/index'
-  get 'test/show'
+
   devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
