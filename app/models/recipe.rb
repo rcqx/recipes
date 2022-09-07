5fc46d01bae81400db7dfdb328ef1926119b265c
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-07 11:24:53 +0500

    Add method ingredients on recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,26 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
 
   validates :name, presence: true, length: { in: 2..75 }
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
+
+  def price
+    price = 0
+    foods.each do |food|
+      price += food.price
+    end
+    price.round(2)
+  end
+
+  def ingredients
+    hash = {}
+    recipe_foods.includes(:food).each do |recipe_food|
+      key = recipe_food.food.name
+      quantity = recipe_food.quantity
+      hash[key] ? hash[key] += quantity : hash[key] = quantity
+    end
+    hash
+  end
 end
+
