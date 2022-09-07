542989950d162bf599753c5a1137c52f53ac9e64
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-07 21:46:03 +0500

    Fix routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   get 'public_recipes', to: 'home#public_recipes'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :foods, only: [:index, :show, :new, :create, :destroy]
