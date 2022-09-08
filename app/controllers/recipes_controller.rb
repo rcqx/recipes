b2fbbefb8edfa3e43568597b45286ad155d3334b
fabianofrank <fabianofrank@protonmail.com>
2022-09-08 15:30:29 -0300

    Add rubocop fixes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,12 @@
 class RecipesController < ApplicationController
   def new; end
   def create; end
+
   def destroy
     Recipe.destroy(params[:id])
     redirect_to '/recipes'
   end
+
   def index
     @recipes = if current_user
                  Recipe.where(user_id: current_user.id)
@@ -12,9 +14,11 @@ class RecipesController < ApplicationController
                  Recipe.all
                end
   end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
+
   def update
     @recipe = Recipe.find(params[:id])
     @recipe.public = @recipe.public == false
