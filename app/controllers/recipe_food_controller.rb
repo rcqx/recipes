aa62fa3a8dc37d75aaae7a52a5bbaca1fcbe15bd
jr-cast <castanos@pm.me>
2022-09-08 01:04:39 -0500

    Add ingrediente form

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -2,9 +2,23 @@ class RecipeFoodController < ApplicationController
   def index
     @recipe_food = RecipeFood.find(params[:recipe_id]);
     @recipe = Recipe.find(params[:recipe_id])
+    @foods = Food.all
   end
 
-  def update; end
+  def create
+    @foods = Food.all
+    @food = Food.find_by(id: params[:food_id])
+    @recipe = Recipe.find_by(id: params[:recipe_id])
+    @recipe_food = RecipeFood.new(quantity: params[:quantity], food: @food, recipe: @recipe)
+
+    if @recipe_food.save
+      flash[:success] = '...created!'
+      redirect_to recipe_path(params[:recipe_id])
+    else
+      flash[:error] = '...with problems, try again...'
+      render :new
+    end
+  end
 
   def destroy
     RecipeFood.destroy(params[:id])
