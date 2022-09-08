a9dfa868433c11186311262b9343ef01fc23ec68
fabianofrank <fabianofrank@protonmail.com>
2022-09-07 22:16:21 -0300

    Fix rubocop

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,7 @@ class Ability
 
   def initialize(user)
     return unless user.present?
+    
     can :delete, Recipe, user: user
     can :delete, Food, user: user
     can :read, Food, user: user
