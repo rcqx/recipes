9f6af69b927c81e9adde2df9bbdbaebde68386e3
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-09 12:18:36 +0500

    Add test file for foods in system

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,49 +1,40 @@
 require 'rails_helper'
 
-describe 'Testing Recipes pages', type: :request do
-#   let(:user) do
-#     user = User.new(first_name: 'Joshua', last_name: 'Ivie', email: 'joshua@joshuaivie.com', password: 'password')
+describe 'Recipes', type: :request do
 
-#     user.password = 'password'
-#     user.password_confirmation = 'password'
-#     user.confirm
-#     user
-#   end new
+  let(:user) do
+    user = User.new(name: 'Fatima', email: 'fatimaazahir@gmail.com', password: '123456')
+    user.password = 'password'
+   user.password_confirmation = 'password'
+   user.confirm
+   user
+  end 
+   
+  subject(:recipe) { user.Recipe.create(name: "Rice & Chicken Stew", preparation_time: 35, cooking_time: 50, 
+   description: "Rice and stew is a popular Nigerian dish made with rice, meat, and vegetables.",
+    is_public: true ) }
+  
+    before(:each) do
+      sign_in(user)
+    end
+  
+    after(:each) do
+      sign_out(user)
+    end
 
-  context 'with valid parameters' do
-    let(:user) do
 
-        { recipe: { user:, name: "Rice & Chicken Stew", preparation_time: 35, cooking_time: 50, 
-        description: "Rice and stew is a popular Nigerian dish made with rice, meat, and vegetables.\n\n It is a staple food in Nigeria and is often served with fried plantains.", public: true } }
-end
-
-# before(:each) { get recipes_path }
-
-    # @recipe = user.Recipe.create(name: "Rice & Chicken Stew", preparation_time: 35, cooking_time: 50, 
-    # description: "Rice and stew is a popular Nigerian dish made with rice, meat, and vegetables.\n\n It is a staple food in Nigeria and is often served with fried plantains.",
-    #  is_public: false, user: )
-
-  before(:each) do
-    sign_in(user)
-  end
-
-  after(:each) do
-    sign_out(user)
-  end
-
-  describe 'GET #index page' do
+  describe 'GET /index' do
+  before(:example) do
     get recipes_path 
+  end
     it 'should return correct response' do
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
     it 'should render the correct template' do
       expect(response).to render_template(:index)
     end
-    it 'should have the text from food name' do
-      expect(response.body).to include('Rice & Chicken Stew')
-    end
-    it 'should have the text from description' do
-      expect(response.body).to include('Rice and stew is a popular Nigerian dish made with rice, meat, and vegetables.\n\n It is a staple food in Nigeria and is often served with fried plantains.')
+    it 'has the correct placeholder text' do
+      expect(response.body).to include('My Recipes')
     end
   end
 end
